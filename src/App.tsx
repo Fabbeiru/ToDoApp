74355efcbb3bd01c6cb11638ef40a8a5edbc2bc5
Fabian B <fabian@secret-source.eu>
2022-04-07 15:08:14 +0100

    Added input field, button and basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <div className='wrapper'>
+        <header>
+          <h1>To Do App</h1>
+        </header>
+
+        <form className='input-form'>
+          <input type="text" placeholder='Write your task here' autoFocus/>
+          <button type="submit" className='input-button'>Save</button>
+        </form>
+      </div>
     </div>
   );
 }
