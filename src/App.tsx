988bfe45d7067cbf6287c192cedd927330a62a2f
Fabian B <fabian@secret-source.eu>
2022-04-08 10:38:39 +0100

    Create, remove and mark as completed functionality added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
+type FormElement = React.FormEvent<HTMLFormElement>;
+
+interface Task {
+  description: string;
+  completed: boolean; 
+}
+
 function App() {
+
+  const [newTask, setNewTask] = useState<string>("");
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const handleSubmit = (e: FormElement) => {
+    e.preventDefault(); // Avoid default behaviour of a form -> reload page
+    addTask(newTask);
+    setNewTask('');
+  }
+
+  const addTask = (description: string) => {
+    const aux: Task[] = [...tasks, {description, completed: false}];
+    setTasks(aux);
+    console.log(aux);
+  }
+
+  const completeTask = (i: number) => {
+    const aux: Task[] = [...tasks];
+    tasks[i].completed = !tasks[i].completed;
+    setTasks(aux);
+  }
+
+  const removeTask = (i: number) => {
+    const aux: Task[] = [...tasks];
+    aux.splice(i, 1);
+    setTasks(aux);
+  }
+
   return (
     <div className="App">
       <div className='wrapper'>
@@ -9,10 +44,24 @@ function App() {
           <h1>To Do App</h1>
         </header>
 
-        <form className='input-form'>
-          <input type="text" placeholder='Write your task here' autoFocus/>
+        <form className='input-form' onSubmit={handleSubmit}>
+          <input type="text" placeholder='Write your task here' autoFocus
+            value={newTask} onChange={(e) => setNewTask(e.target.value)}/>
           <button type="submit" className='input-button'>Save</button>
         </form>
+
+        <div className="task-wrapper">
+          {
+            tasks.map((t: Task, i: number) => (
+              <div className="task" key={i}>
+                <h2 style={{textDecoration: t.completed ? 'line-through' : ''}}>{t.description}</h2>
+                <p>{t.completed + ''}</p>
+                <button className="complete-task" onClick={() => completeTask(i)}>✓</button>
+                <button className="erase-task" onClick={() => removeTask(i)}>✗</button>
+              </div>
+            ))
+          }
+        </div>
       </div>
     </div>
   );
